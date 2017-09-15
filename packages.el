b219be79583d3816ccf3bedcad0d0e839e672f43
daniel nakov <daniel.nakov@gmail.com>
2017-09-14 22:46:02 -0400

    initial

---
+++ packages.el
@@ -0,0 +1,20 @@
+(setq use-package-always-ensure t)
+(use-package dracula-theme)
+(use-package projectile)
+(use-package ivy
+  :config
+  (define-key ivy-minibuffer-map (kbd "<left>") 'counsel-up-directory)
+  (define-key ivy-minibuffer-map (kbd "<right>") 'ivy-alt-done))
+;;  (ivy-set-sources 'ivy-switch-buffer '((original-source) 'counsel-file-jump)))
+(use-package ivy-rich
+;;  :defer t
+  :load-path "~/.emacs.d/lisp/ivy-rich.el")
+(use-package counsel-projectile)
+(use-package counsel)
+(use-package smartscan)
+(use-package rjsx-mode
+  :mode ("\\.js\\'" . rjsx-mode)
+  :config
+  (setq js2-strict-missing-semi-warning nil)
+  (setq js-indent-level 2)
+  (setq js2-missing-semi-one-line-override nil))
